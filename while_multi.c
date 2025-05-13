ab44bad65953da6180e591437cd4fcfcda8c88c9
ANUSHA-2006-G <anushaeshwaran@gmail.com>
2025-05-13 12:14:36 +0530

    Create while_multi.c

---
+++ while_multi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n,i=1;
+    printf("Enter the value of n");
+    scanf("%d",&n);
+    
+    while(i<=10){
+    printf("%d*%d=%d\n",n,i,n*i);  
+    i++;
+    }
+}
+Enter the value of n6
+6*1=6
+6*2=12
+6*3=18
+6*4=24
+6*5=30
+6*6=36
+6*7=42
+6*8=48
+6*9=54
+6*10=60
+
+
+=== Code Execution Successful ===
