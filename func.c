647436c376dbea33e6d11b1a25d6470830931d97
ANUSHA-2006-G <anushaeshwaran@gmail.com>
2025-05-13 15:26:17 +0530

    Create func.c

---
+++ func.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int add(int a,int b)
+{
+//function declaration
+int c=a+b;
+return c;
+}
+int main()
+{
+int result=add(5,10); //function calling
+printf("Result is: %d",result);
+return 0;
+}
+Result is: 15
+
+=== Code Execution Successful ===
