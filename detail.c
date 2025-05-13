6406160323d040350a49a9f62cf35367b576347e
ANUSHA-2006-G <anushaeshwaran@gmail.com>
2025-05-13 15:38:40 +0530

    Create detail.c

---
+++ detail.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int shridevi()
+{
+ printf("Anusha\n");
+ printf("04-12-2006\n");
+ printf("19\n");
+ printf("7338496517\n");
+ 
+}
+int main()
+{
+    shridevi();
+    return 0;
+}
+OUTPUT
+Anusha
+04-12-2006
+19
+7338496517
+
+
+=== Code Execution Successful ===
