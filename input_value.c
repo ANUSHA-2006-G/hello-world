d7f68fa180f91928cd3c88c57d4bedff30318ce4
ANUSHA-2006-G <anushaeshwaran@gmail.com>
2025-05-13 12:40:31 +0530

    Create input_value.c

---
+++ input_value.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("enter the two interges:");
+    scanf("%d %d",&a,&b);
+    printf("Sum=%d\n",a+b);
+    return 0;
+    }
+enter the two interges:7 2
+Sum=9
+
+
+=== Code Execution Successful ===
