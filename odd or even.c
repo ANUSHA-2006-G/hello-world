55fc7dadf0f62230acf457a00d8555ac9e9cc307
ANUSHA-2006-G <anushaeshwaran@gmail.com>
2025-05-13 10:12:29 +0530

    Create odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  int a;
+  printf("enter the value of a");
+  scanf("%d",&a);
+  if(a>0);
+  printf("the number %d is postivie");
+  }
+enter the value of a4
+the number 4 is postivie
+
+=== Code Execution Successful ===
