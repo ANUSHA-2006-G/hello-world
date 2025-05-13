c08975b1a0c47a281879dc0b72a6ee43737332c4
ANUSHA-2006-G <anushaeshwaran@gmail.com>
2025-05-13 10:28:37 +0530

    Create marks.c

---
+++ marks.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+  int marks;
+  printf("enter your marks:\n");
+  scanf("%d",&marks);
+  if(marks>=80)
+  {
+    printf("Grade:A\n");
+  }else if(marks>=75){
+      printf("Grade:B");
+  }else if(marks>=60){
+  printf ("Grade;c");
+  }else{
+      printf("Grade;f");
+  }
+  }
+enter your marks:
+85
+Grade:A
+
+
+=== Code Execution Successful ===
