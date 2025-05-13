87c17af29c0be065446d08d86244aec72e10f229
ANUSHA-2006-G <anushaeshwaran@gmail.com>
2025-05-13 16:08:33 +0530

    Create passorfail.c

---
+++ passorfail.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int marks;
+int main(){
+    printf("Enter the marks:");
+    scanf("%d",&marks);
+    if (marks>=40)
+{
+    printf("passed");
+}
+else{
+    printf("failed");
+
+}
+}
