d1f4c0c3a64d9cbc6a1876a196153d4c57b746df
ANUSHA-2006-G <anushaeshwaran@gmail.com>
2025-05-13 13:05:12 +0530

    Create name,age,greeting.c

---
+++ name,age,greeting.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+ char name[50];
+ int age;
+ printf("Enter your name and ages: ");
+ scanf("%s %d",name,&age);
+ printf("Hello %s,you are %d years old.\n",name,age);
+return 0;
+}
+Enter your name and ages: anu 20
+Hello anu,you are 20 years old.
+
+
+=== Code Execution Successful ===
