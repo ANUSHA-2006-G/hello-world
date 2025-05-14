3ffc3f4d963228aaa4605874fe55b79886f360d4
ANUSHA-2006-G <anushaeshwaran@gmail.com>
2025-05-14 15:14:59 +0530

    Create arraysum.c

---
+++ arraysum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int n,arr[n],sum =0,i;
+    printf("enter the array elementns n");
+    scanf("%d",&n);
+    printf("enter the elements\n");
+    for(i=0;i<n;i++){
+        scanf("%d", &arr[n]);
+        sum +=arr[n];
+        
+    }
+    printf("%d\n",sum);
+    
+}
+enter the array elementns n5
+enter the elements
+1
+2
+3
+4
+5
+15
+
+
+=== Code Execution Successful ===
