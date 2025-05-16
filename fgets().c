2e4e98ade386be282cd395761736f277673359da
ANUSHA-2006-G <anushaeshwaran@gmail.com>
2025-05-16 09:20:43 +0530

    Create fgets().c

---
+++ fgets().c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char name[10];
+    printf("Enter your name:");
+fgets(name,10,stdin);
+printf("your name is:%s",name);
+    return 0;
+}
+Enter your name:anusha
+your name is:anusha
+
+
+=== Code Execution Successful ===
